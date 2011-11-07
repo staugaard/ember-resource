fc0a2c7dacc32d8aece1a65af0a5d026b4441beb
Mick Staugaard <mick@staugaard.com>
2011-11-07 14:40:47 -0800

    forgot to check this test in

---
+++ spec/javascripts/saveSpec.js
@@ -0,0 +1,124 @@
+describe('Saving a resource instance', function() {
+  var Model, model, server;
+
+  beforeEach(function() {
+    Model = SC.Resource.define({
+      schema: {
+        id:       Number,
+        name:     String,
+        subject:  String
+      },
+      url: '/people'
+    });
+
+    server = sinon.fakeServer.create();
+  });
+
+  afterEach(function() {
+    server.restore();
+    SC.Resource.errorHandler = null;
+  });
+
+  describe('handling errors on save', function() {
+    beforeEach(function() {
+      server.respondWith('POST', '/people', [422, {}, '[["foo", "bar"]]']);
+    });
+
+    it('should pass a reference to the resource to the error handling function', function() {
+      var spy = jasmine.createSpy();
+      SC.Resource.errorHandler = function(a, b, c, fourthArgument) {
+        spy(fourthArgument);
+      };
+
+      var resource = Model.create({ name: 'foo' });
+      resource.save();
+      server.respond();
+
+      expect(spy).toHaveBeenCalledWith(resource);
+    });
+  });
+
+  describe('resourceState', function() {
+    describe('saving', function() {
+      var resource;
+
+      beforeEach(function() {
+        server.respondWith('POST', '/people', [201, {}, '']);
+        resource = Model.create({ name: 'foo' });
+        expect(resource.get('resourceState')).not.toBe(SC.Resource.Lifecycle.SAVING);
+      });
+
+      it('should change to the saving state while saving', function() {
+        expect(resource.save()).toBeTruthy();
+        expect(resource.get('resourceState')).toBe(SC.Resource.Lifecycle.SAVING);
+      });
+
+      it('should change to previous state after save completes', function() {
+        var previousState = resource.get('resourceState');
+        expect(resource.save()).toBeTruthy();
+        expect(resource.get('resourceState')).not.toBe(previousState);
+        server.respond();
+        expect(resource.get('resourceState')).toBe(previousState);
+      });
+
+      it('should not allow concurrent saves', function() {
+        expect(resource.save()).toBeTruthy();
+        expect(resource.save()).toBe(false);
+        server.respond();
+        expect(resource.save()).toBeTruthy();
+      });
+    });
+
+  });
+
+  describe('updating from response', function() {
+      var resource;
+
+      describe('with default Location header parsing', function() {
+        beforeEach(function() {
+          server.respondWith('POST', '/people', [201, {'Location': 'http://example.com/people/25.json'}, '']);
+          resource = Model.create({ name: 'foo' });
+        });
+
+        it('should update with the id from the Location header', function() {
+          resource.save();
+          server.respond();
+          expect(resource.get('id')).toBe(25);
+        });
+      });
+
+      describe('with a custom Location header parser', function() {
+        beforeEach(function() {
+          server.respondWith('POST', '/people', [201, {'Location': 'http://example.com/people/25.json'}, '']);
+          Model.reopenClass({
+            idFromURL: function(url) {
+              return 100;
+            }
+          });
+
+          resource = Model.create({ name: 'foo' });
+        });
+
+        it('should update with the id from the custom parser', function() {
+          resource.save();
+          server.respond();
+          expect(resource.get('id')).toBe(100);
+        });
+      });
+
+      describe('from a response body', function() {
+        beforeEach(function() {
+          server.respondWith('POST', '/people', [201, { "Content-Type": "application/json" }, '{ "id": 1, "subject": "the subject" }']);
+          resource = Model.create({ name: 'foo' });
+        });
+
+        it('should update with the data given', function() {
+          resource.save();
+          server.respond();
+          expect(resource.get('id')).toBe(1);
+          expect(resource.get('subject')).toBe('the subject');
+          expect(resource.get('name')).toBe('foo');
+        });
+      });
+  });
+});
