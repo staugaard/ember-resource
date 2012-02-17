289769e33c7fd095838abaf03a84c51941fbecc8
Josh Lubaway <josh.lubaway@gmail.com>
2012-02-16 16:49:59 -0800

    ResourceCollection will read it's `url` property.
    Previously ResourceCollection was only looking at `this.url`.
    However, if the url is defined as a computed property
    ResourceCollection would totally overlook it. Now it will check the
    computed property as well.

---
+++ spec/javascripts/resourceCollectionSpec.js
@@ -0,0 +1,21 @@
+/*globals Em*/
+
+describe('ResourceCollection', function() {
+
+  it("reads from it's url property if present", function() {
+    var Model = Em.Resource.define({
+      url: '/url/from/resource'
+    });
+
+    var collection = Em.ResourceCollection.create({
+      type: Model,
+
+      url: function() {
+        return '/foo';
+      }.property().cacheable()
+    });
+
+    expect(collection.resolveUrl()).toEqual('/foo');
+  });
+
+});
