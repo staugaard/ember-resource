bfe4301abd802fbabb8b7aa3238ed23a583261f6
Saroj Yadav <syadav@zendesk.com>
2012-03-27 12:04:22 -0700

    For a resource, update its underlying data only if the API data is different than its current data.

--- spec/javascripts/deepMergeSpec.js
+++ spec/javascripts/deepMergeSpec.js
@@ -15,6 +15,29 @@ describe('deepMerge', function() {
     expect(obj.a).toBe('bar');
   });
 
+  it('should not override keys if values are same', function() {
+    var obj = {a: 'foo'};
+    spyOn(Ember, 'set');
+    Ember.Resource.deepMerge(obj, {a: 'foo'});
+    expect(Ember.set).not.toHaveBeenCalledWith(obj, 'a', 'foo');
+  });
+
+  it('should not override keys if values are empty arrays', function() {
+    var obj = {a: []};
+    spyOn(Ember, 'set');
+    Ember.Resource.deepMerge(obj, {a: []});
+    expect(Ember.set).not.toHaveBeenCalledWith(obj, 'a', []);
+  });
+
+  it('should override keys if values are not empty arrays', function() {
+    var obj = {a: [2]};
+    Ember.Resource.deepMerge(obj, {a: [3]});
+    expect(obj.a).toBeDefined();
+    expect(obj.a.length).toBe(1);
+    expect(obj.a[0]).toBe(3);
+  });
+
+
   it('should leave other keys', function() {
     var obj = {a: 'foo'};
     Ember.Resource.deepMerge(obj, {b: 'bar'});
