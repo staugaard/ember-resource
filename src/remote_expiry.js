cd00ace072d81200b80891292c9e9763a4c32f9e
Josh Lubaway <josh.lubaway@gmail.com>
2013-02-04 17:52:50 -0800

    Fixing jshint errors.
    * Added a package.json to install a local copy of jshint
    * Removed all of the deprecated jshint options in `.jshintrc`
    * Linted all of the source files
    
    You can currently run jshint on the project using:
    
        npm install
        ./node_modules/jshint/bin/hint src/*.js
    
    NOTE: Most of the errors are whitespace related. I do not know if we
    actually want to make the whitespace changes or not.

--- src/remote_expiry.js
+++ src/remote_expiry.js
@@ -1,4 +1,4 @@
-(function(exports) {
+(function (exports) {
 
   var Ember = exports.Ember, NullTransport = {
     subscribe: Ember.K,
@@ -8,49 +8,49 @@
   Ember.Resource.PushTransport = NullTransport;
 
   var RemoteExpiry = Ember.Mixin.create({
-    init: function() {
+    init: function () {
       var ret = this._super(),
           self = this,
           remoteExpiryScope = this.get('remoteExpiryKey');
 
       this.set('_subscribedForExpiry', false);
 
-      if(!remoteExpiryScope) {
+      if (!remoteExpiryScope) {
         return ret;
       }
 
-      Ember.addListener(this, 'didFetch', this, function() {
+      Ember.addListener(this, 'didFetch', this, function () {
         self.subscribeForExpiry();
       });
 
       return ret;
     },
 
-    subscribeForExpiry: function() {
+    subscribeForExpiry: function () {
       var remoteExpiryScope = this.get('remoteExpiryKey'),
           self = this;
 
-      if(!remoteExpiryScope) {
+      if (!remoteExpiryScope) {
         return;
       }
 
-      if(this.get('_subscribedForExpiry')) {
+      if (this.get('_subscribedForExpiry')) {
         return;
       }
 
-      Ember.Resource.PushTransport.subscribe(remoteExpiryScope, function(message) {
+      Ember.Resource.PushTransport.subscribe(remoteExpiryScope, function (message) {
         self.updateExpiry(message);
       });
 
       this.set('_subscribedForExpiry', true);
     },
 
-    updateExpiry: function(message) {
+    updateExpiry: function (message) {
       var updatedAt = message && message.updatedAt;
-      if(!updatedAt) return;
-      if(this.stale(updatedAt)) {
+      if (!updatedAt) return;
+      if (this.stale(updatedAt)) {
         this.set('expiryUpdatedAt', updatedAt);
-        if(this.get('remoteExpiryAutoFetch')) {
+        if (this.get('remoteExpiryAutoFetch')) {
           this.set('isExpired', true);
           this.fetch();
         } else {
@@ -59,7 +59,7 @@
       }
     },
 
-    stale: function(updatedAt) {
+    stale: function (updatedAt) {
       return !this.get('expiryUpdatedAt') || (+this.get('expiryUpdatedAt') < +updatedAt);
     }
   });
